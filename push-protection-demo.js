f918f566180eb4113b0c4a49d83a7d386b2063d9
Len Volk <lvolkov@outlook.com>
2026-02-25 09:41:09 -0500

    demo: add config with API token

---
+++ push-protection-demo.js
@@ -0,0 +1,10 @@
+// Demo file to trigger GitHub Push Protection
+// This file contains a test secret pattern
+
+const config = {
+    // This fake token pattern should trigger push protection
+    GITHUB_TOKEN: "ghp_wWPw5k4aXcaT4fNP0UcnZwJUVFk6LO2rINYo",
+    API_ENDPOINT: "https://api.example.com"
+};
+
+module.exports = config;
